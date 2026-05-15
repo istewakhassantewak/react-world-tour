9a3ed14b00baa442008dac653c80c2843ce69076
Istewak Hassan Tewak <121757458+istewakhassantewak@users.noreply.github.com>
2026-05-15 22:52:46 +0600

    styling and showing country data and flag image from API

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,20 +1,22 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
-
+import './Countries.css'
 const Countries = () => {
     const [countries, setCountries]=useState([])
     useEffect(()=>{
-        fetch('https://restcountries.com/v3.1/all?fields=name,capital,currencies,flags,cca3')
+        fetch('https://restcountries.com/v3.1/all?fields=name,capital,currencies,flags,cca3,population,area')
         .then(res=>res.json())
         .then(data=>setCountries(data))
     },[])
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
-            {
+            <div className="country-container">
+                {
                 countries.map(country=><Country 
                     key={country.cca3} country={country}></Country>)
             }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,13 @@
 import './Country.css'
 const Country = ({country}) => {
     console.log(country)
-    const {name, flags}=country
+    const {name, flags,population,area}=country
     return (
         <div className="country">
            <h3>name: {name?.common || "Missing"}</h3> 
            <img src={flags.png} alt="img" />
+           <p>Population: {population}</p>
+           <p>area: {area}</p>
         </div>
     );
 };
