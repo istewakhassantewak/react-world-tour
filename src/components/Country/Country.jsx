8d983a68d7c8a3651cd9ec5f91d5db049461d1fc
Istewak Hassan Tewak <121757458+istewakhassantewak@users.noreply.github.com>
2026-05-15 23:25:03 +0600

    visited design and implementation

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = ({country}) => {
     console.log(country)
-    const {name, flags,population,area}=country
+    const {name, flags,population,area,cca3}=country
+    const [visited, setVisited]=useState(false)
+    const handleVisited=()=>{
+        setVisited(!visited)
+    }
     return (
-        <div className="country">
-           <h3>name: {name?.common || "Missing"}</h3> 
+        <div className={`country ${visited  && 'visited'}`}>
+           <h3 style={{color: visited ? 'Purple' : 'white'}}>name: {name?.common || "Missing"}</h3> 
            <img src={flags.png} alt="img" />
            <p>Population: {population}</p>
-           <p>area: {area}</p>
+           <p><small>area: {area}</small></p>
+           <p>Code: {cca3}</p>
+           <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
+           {visited ? 'I have visited this country.' : 'I want to visit'}
         </div>
     );
 };
